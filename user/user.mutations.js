7c38b363591394854b74e616b7ab1cd73a052796
bungaepyo <bungaepyo@gmail.com>
2021-05-31 13:58:03 -0400

    createAccount

--- user/user.mutations.js
+++ user/user.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt, { hash } from "bcrypt";
 import client from "../client";
 
 export default {
@@ -6,17 +7,36 @@ export default {
       _,
       { firstName, lastName, userName, email, password, }
     ) => {
-      // check if userName and email are unique
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            { userName }, { email }
-          ]
-        }
-      });
-      // hash password
-      // save and return
+      try {
+        // check if userName and email are unique
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              { userName }, { email }
+            ]
+          }
+        });
 
+        if (existingUser) {
+          throw new Error("The username or email already exists.");
+        }
+
+        // hash password
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        // save and return
+        return client.user.create({
+          data: {
+            userName,
+            email,
+            firstName,
+            lastName,
+            password: hashPassword
+          }
+        })
+      } catch (e) {
+        return e;
+      }
     }
   }
 }
