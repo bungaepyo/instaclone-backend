fec052e6903ba37617d3213f754064ce7ff74e9c
bungaepyo <bungaepyo@gmail.com>
2021-06-10 21:57:38 -0400

    add functional protectedResolver

--- user/editProfile/editProfile.resolvers.js
+++ user/editProfile/editProfile.resolvers.js
@@ -1,37 +1,39 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import { protectedResolver } from "../user.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, userName, email, password: newPassword },
-      { loggedInUser }
-    ) => {
-      console.log(loggedInUser);
-      let hashPassword = null;
-      if (newPassword) {
-        hashPassword = await bcrypt.hash(newPassword, 10);
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, userName, email, password: newPassword },
+        { loggedInUser }
+      ) => {
+        protectedResolver(loggedInUser);
+        let hashPassword = null;
+        if (newPassword) {
+          hashPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id
+          },
+          data: {
+            firstName, lastName, userName, email, ...(hashPassword && { password: hashPassword }),
+          }
+        });
+        if (updatedUser.id) {
+          return {
+            success: true
+          }
+        } else {
+          return {
+            success: false,
+            error: "Cannot update profile."
+          }
+        }
       }
-      const updatedUser = await client.user.update({
-        where: {
-          id: loggedInUser.id
-        },
-        data: {
-          firstName, lastName, userName, email, ...(hashPassword && { password: hashPassword }),
-        }
-      });
-      if (updatedUser.id) {
-        return {
-          success: true
-        }
-      } else {
-        return {
-          success: false,
-          error: "Cannot update profile."
-        }
-      }
-    },
+    ),
   }
 }
--- user/user.utils.js
+++ user/user.utils.js
@@ -17,3 +17,13 @@ export const getUser = async (token) => {
     return null;
   }
 }
+
+export const protectedResolver = (resolver) => (root, args, context, info) => {
+  if (!context.loggedInUser) {
+    return {
+      success: false,
+      error: "Not Logged In.",
+    }
+  }
+  return resolver(root, args, context, info);
+}
