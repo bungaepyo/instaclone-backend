e0fb05573f9277b642ecd306d18cec04a7af431e
bungaepyo <bungaepyo@gmail.com>
2021-06-10 20:55:26 -0400

    editProfile & use token to verify

--- user/editProfile/editProfile.resolvers.js
+++ user/editProfile/editProfile.resolvers.js
@@ -1,5 +1,37 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("HI")
+    editProfile: async (
+      _,
+      { firstName, lastName, userName, email, password: newPassword },
+      { loggedInUser }
+    ) => {
+      console.log(loggedInUser);
+      let hashPassword = null;
+      if (newPassword) {
+        hashPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id: loggedInUser.id
+        },
+        data: {
+          firstName, lastName, userName, email, ...(hashPassword && { password: hashPassword }),
+        }
+      });
+      if (updatedUser.id) {
+        return {
+          success: true
+        }
+      } else {
+        return {
+          success: false,
+          error: "Cannot update profile."
+        }
+      }
+    },
   }
 }
